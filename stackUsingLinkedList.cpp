5562e7846a40c8808282be6086439167c985f172
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-05-20 10:38:43 +0700

    case 3

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -106,6 +106,19 @@ int main()
                     << endl;
             }
             break;
+        case 3 :
+            if(!stact.isEmpty())
+            {
+                stact.peek();
+            }
+            else
+            {
+                cout << "Stact is Empty. No top value."
+                    << endl;
+            }
+            break;
+        
+
         
         
             
