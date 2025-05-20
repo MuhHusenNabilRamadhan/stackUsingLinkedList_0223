19724ed350f8b4a877d6a5dc7fe947709ae5d247
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-05-20 10:06:14 +0700

    int value

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -25,5 +25,14 @@ class Stack // nama kelas
 
             top = NULL;
         }
-
+        
+         int push (int value)
+        {
+            Node *newNode = new Node();
+            newNode-> data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << " Push Value : " << value << endl;
+            return value;
+        }
 };
