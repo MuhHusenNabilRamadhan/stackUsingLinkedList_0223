90ef1347e45ab17c58e463902c1a043cf7b9836f
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-05-20 10:17:27 +0700

    Tampilkan semua elemen stack dari atas ke bawah

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -47,12 +47,23 @@ class Stack // nama kelas
             top = top->next;
         }
 
-        
         void peek()
         {
             if(top == NULL)
             {
                 cout << "List is Empty. " << endl;
+            }
+            
+            else
+            {
+                Node *current = top;
+                while (current != NULL)
+                {
+                    cout << current->data << " " << endl;
+                    current = current->next;
+                }
+                cout << endl;
+
             }
         }
 
